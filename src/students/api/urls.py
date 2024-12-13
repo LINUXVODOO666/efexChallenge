0ce5125cb172e5e0fdd18099cd052589943ddcf7
maynex <mayneznoe@gmail.com>
2024-12-13 00:19:16 -0600

    static files

--- src/students/api/urls.py
+++ src/students/api/urls.py
@@ -4,5 +4,4 @@ from students.api.views import Student, StudentById
 urlpatterns = [
     path('students/', Student.as_view(),name='students'),
     path('students/<int:student_id>/', StudentById.as_view(),name='studentId'),
-
 ]
